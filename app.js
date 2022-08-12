c63ffaac1444ece89b5dbdbf3092d7972c04b27b
是小韩啊 <1655466387@qq.com>
2022-08-12 13:35:25 +0800

    添加更新提醒功能

--- app.js
+++ app.js
@@ -2,16 +2,25 @@
 App({
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+    // 微信小程序获取版本更新
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
     })
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '小韩提示',
+        content: '新版来袭，速来体验！',
+        success: function (res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    });
   },
   globalData: {
     userInfo: null
